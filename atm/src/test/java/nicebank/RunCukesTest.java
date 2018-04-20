d7f5e2bf7c7defdba85941e5e715564e8e593fbd
jchdez1984 <jc_hdez_automation@outlook.com>
2018-04-20 14:57:59 -0500

    Update RunCukesTest.java

--- atm/src/test/java/nicebank/RunCukesTest.java
+++ atm/src/test/java/nicebank/RunCukesTest.java
@@ -5,6 +5,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty", "junit:target/cucumber-reports/Cucumber.xml"})
+@CucumberOptions(plugin = {"pretty", "junit:target/Cucumber.xml"})
 public class RunCukesTest {
 }
